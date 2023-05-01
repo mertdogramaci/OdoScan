5077317ab59540970ce0ca8870e2cdc8eb627b58
mertdogramaci <mertdogramaci@gmail.com>
2023-05-01 23:23:15 +0300

    start screen has made

---
+++ src/lib/start_screen.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+class StartScreen extends StatelessWidget {
+  const StartScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topLeft,
+          colors: [Colors.indigo, Colors.deepPurple],
+          end: Alignment.bottomRight,
+        ),
+      ),
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Opacity(
+              opacity: 0.8,
+              child: Image.asset(
+                'assets/images/speedometer-clipart-design-illustration-free-png.webp',
+                width: 200,
+              ),
+            ),
+            const SizedBox(height: 30),
+            const Text(
+              'OdoScan',
+              style: TextStyle(
+                  fontSize: 32,
+                  fontWeight: FontWeight.w400,
+                  color: Color.fromARGB(255, 237, 223, 252)),
+            ),
+            const SizedBox(height: 80),
+            OutlinedButton.icon(
+              onPressed: () {},
+              style: OutlinedButton.styleFrom(
+                foregroundColor: Colors.white,
+              ),
+              icon: const Icon(Icons.arrow_forward_ios),
+              label: const Text('Get Started'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
