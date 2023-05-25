ecea417d838492e909552f9354ed72b7e22c17b6
mertdogramaci <mertdogramaci@gmail.com>
2023-05-25 16:53:15 +0300

    initial commit

---
+++ backend/OdoScan/src/test/java/com/rockofmam/OdoScan/OdoScanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rockofmam.OdoScan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OdoScanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
